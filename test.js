77bae5a5192e895a21026c6f8f15ececca181655
dkw <871570594@qq.com>
2019-08-17 23:45:55 +0800

    修改

--- test.js
+++ test.js
@@ -2,6 +2,8 @@
 //test
 //栈操作
 function pushStack(elems){
+	//init{} prototype=>length:0
+	//
 	var ret = JQuery.merge(this.constructor(),elems);
 		ret.prevObject = this;
 		ret.context = this.context;
@@ -9,6 +11,7 @@ function pushStack(elems){
 }
 //merge
 //伪数组 数组{0：a,1:b,length:2}
+//first必须有length这个属性且为num
 function merge(first,second){
 		
 		var l = second.length,

3a1834bdde3e8d15ae83c9bfc516f85dad92ca5d
dkw <871570594@qq.com>
2019-08-17 18:39:47 +0800

    提交到分支

--- test.js
+++ test.js
@@ -6,6 +6,25 @@ function pushStack(elems){
 		return ret;
 }
 //merge
-function merge(){
+function merge(first,second){
 	
+		var l = second.length,
+			i = first.length,
+			j = 0;
+
+		if ( typeof l === "number" ) {
+			for ( ; j < l; j++ ) {
+				first[ i++ ] = second[ j ];
+			}
+		} else {
+			//内部使用
+			while ( second[j] !== undefined ) {
+				first[ i++ ] = second[ j++ ];
+			}
+		}
+
+		first.length = i;
+
+		return first;
+		
 }
